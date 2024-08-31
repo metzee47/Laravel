d4c57d7e4634fa80f56fb887f4011c07cf0bd0ca
metzee47 <syllamouhamadou351@gmail.com>
2024-08-31 16:54:43 +0100

    Set notification index instructor page(Home)

--- E-learning/app/Http/Controllers/Users/InstructorController.php
+++ E-learning/app/Http/Controllers/Users/InstructorController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers\Users;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\NotificationResource;
+use App\Models\Notification;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
@@ -23,7 +25,13 @@ class InstructorController extends Controller
     public function home(){
 
         $msg = self::msg();
-        return inertia('Users/Instructor/Home', compact('msg'));
+        $notifications = NotificationResource::collection(
+            Notification::query()
+                ->orderBy('created_at', 'desc')
+                ->where('to', Auth::id())
+                ->get()
+        );
+        return inertia('Users/Instructor/Home', compact('msg', 'notifications'));
 
     }
 }
--- E-learning/resources/js/Pages/Users/Instructor/Home.jsx
+++ E-learning/resources/js/Pages/Users/Instructor/Home.jsx
@@ -1,18 +1,74 @@
+import NotificationCard from "@/Components/NotificationCard";
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
 import Body from "@/Layouts/Body";
-import { Head } from "@inertiajs/react";
+import { Head, useForm } from "@inertiajs/react";
+import { useEffect, useState } from "react";
 
-export default function Home({auth, msg}){
+export default function Home({auth, msg, notifications}){
+
+    notifications = notifications.data
+    const fromStudent = notifications.filter(notification=> notification.from.role === 'etudiant')
+    const {data, setData, post} = useForm({})
+    const [checkbox, setCheckbox] = useState({})
+    const [showStudentNotif, setShowStudentNotif] = useState(true)
+
+
+    useEffect(()=>{
+        setCheckbox(()=>
+        {
+            let initialValues = {}
+            notifications.map(notification=>{
+                initialValues[`notif${notification.id}`] = {
+                    id: notification.id,
+                    checked: false
+                }
+            })
+
+            return initialValues
+        })
+    },[])
+
+    useEffect(()=>{
+        const notifReaded = Object.keys(checkbox).filter(notif=> checkbox[notif].checked === true)
+        setData(()=>{
+            let data = {}
+            notifReaded.map(notif=> data[notif] = checkbox[notif].id)
+            return data
+        }) 
+        
+    },[checkbox])
+
+    const markAsRead = () => {
+        post(route('dashboard.delete-notif'))
+    }
     return (
         <AuthenticatedLayout
             user={auth.user}
            
         >
-            <Head title={`Student | ${auth.user.name}`}  />
+            <Head title={`Instructor | Notifications`}  />
 
 
             <Body msg={msg.warning}>
-                I am Professor Men
+            <div className=" w-full bg-gray-900 p-3">
+                <div className='text-xl p-2 bg-gray-950 rounded-md'>NOTIFICATIONS</div>
+                <div>
+                    <div className="flex justify-between">
+                        <h4 onClick={()=>setShowStudentNotif(!showStudentNotif)} className='text-base cursor-pointer my-3 py-1 px-2 rounded-lg w-fit bg-emerald-950 '>From Students</h4>
+                        
+                        { 
+                            showStudentNotif &&
+                            <h4 onClick={markAsRead} className=' hover:scale-95 text-base cursor-pointer my-3 py-1 px-2 rounded-lg w-fit bg-emerald-950'>Marquer comme lu(s)</h4>
+                            
+                        }
+                    </div>
+                    {
+                        showStudentNotif &&
+                        <NotificationCard notifications={fromStudent} checkbox={checkbox} setCheckbox={setCheckbox} setData={setData}/>
+                    }
+            
+                </div>
+            </div>
             </Body>
         </AuthenticatedLayout>
     )
